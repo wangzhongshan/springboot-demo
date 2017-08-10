fa9fd4fb0a5dd6b6d70204fd6438af1e33790d3c
wzs <zhongshan.wang@mtime.com>
2017-08-10 15:07:48 +0800

    up

--- src/main/java/org/wzs/springbootdemo/controller/HomeController.java
+++ src/main/java/org/wzs/springbootdemo/controller/HomeController.java
@@ -2,15 +2,20 @@ package org.wzs.springbootdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HomeController {
     /**
      * 首页
+     *
      * @return
      */
     @GetMapping("/")
-    public String index() {
-        return "hello,wzs!";
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        mv.addObject("userName", "wzs");
+        return mv;
     }
 }
