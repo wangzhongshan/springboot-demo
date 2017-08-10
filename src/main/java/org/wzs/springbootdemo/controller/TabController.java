f63c3a853b755b17ba0e475a33961ad41ffb460a
wzs <zhongshan.wang@mtime.com>
2017-08-10 11:24:35 +0800

    user thymeleaf template

--- src/main/java/org/wzs/springbootdemo/controller/TabController.java
+++ src/main/java/org/wzs/springbootdemo/controller/TabController.java
@@ -1,21 +1,24 @@
 package org.wzs.springbootdemo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("tab")
 public class TabController {
 
     /**
      * tab详情
+     *
      * @param tab
      * @return
      */
     @GetMapping("{tab}")
-    public String tab(@PathVariable String tab) {
+    public String tab(@PathVariable String tab, ModelMap modelMap) {
+        modelMap.addAttribute("tabName", tab);
         return tab;
     }
 }
