5ce0a6c7c9763ff4832b3bc53e662de2f685b694
wzs <zhongshan.wang@mtime.com>
2017-08-11 14:49:06 +0800

    add devtools for develop

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-## springboot demo
+## spring boot demo (out of the box)
 
 * 使用 thymeleaf 模板
 * 使用 [程序猿DD spring-boot-starter-swagger](https://github.com/dyc87112/spring-boot-starter-swagger) 作为接口文档。*(localhost:8080/swagger-ui.html)*
+* 使用 spring data jpa 操作db（mysql）(底层使用hibernate，较简洁轻量)
+* 全局异常处理，自定义异常处理
--- src/main/java/org/wzs/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/org/wzs/springbootdemo/SpringbootDemoApplication.java
@@ -3,6 +3,7 @@ package org.wzs.springbootdemo;
 import com.didispace.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @EnableSwagger2Doc
 @SpringBootApplication
 public class SpringbootDemoApplication {
@@ -10,4 +11,19 @@ public class SpringbootDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
+
+    // @Bean
+    // public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        // return args -> {
+        //
+        //     System.out.println("Let's inspect the beans provided by Spring Boot:");
+        //
+        //     String[] beanNames = ctx.getBeanDefinitionNames();
+        //     Arrays.sort(beanNames);
+        //     for (String beanName : beanNames) {
+        //         System.out.println(beanName);
+        //     }
+        //
+        // };
+    // }
 }
