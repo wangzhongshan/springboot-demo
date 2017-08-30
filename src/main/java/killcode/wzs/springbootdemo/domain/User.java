3381173184994eeb71f88d87c7dd1d9dddd843ad
wzs <zhongshan.wang@mtime.com>
2017-08-30 10:02:54 +0800

    up swagger starter

--- src/main/java/killcode/wzs/springbootdemo/domain/User.java
+++ src/main/java/killcode/wzs/springbootdemo/domain/User.java
@@ -1,23 +1,36 @@
 package killcode.wzs.springbootdemo.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel("用户基本信息")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     @Column(nullable = false)
+    @NotNull
+    @ApiModelProperty("名称")
     private String name;
+
     @Column(nullable = false)
+    @Max(200)
+    @Min(1)
+    @ApiModelProperty("年龄")
     private Integer age;
 }
